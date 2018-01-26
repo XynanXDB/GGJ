0298ded9ff18c22321ac98797501bb3e8a56ee69
XynanXDB <32987787+XynanXDB@users.noreply.github.com>
2018-01-27 00:22:43 +0800

    Push 2

---
+++ The Blimp Prophecy/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	private static bool _bIsStartGame = false;
+	public static bool bIsStartGame { get { return _bIsStartGame;} set { _bIsStartGame = value;} }
+
+	public static bool bIsDefend = false;
+
+	public GameObject player1Blimp;
+	public GameObject player2Blimp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
